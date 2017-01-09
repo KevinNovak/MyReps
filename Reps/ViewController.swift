b94173938347677f83bb1f8a6209a3abb0440282
Kevin Novak <kevinmnovak@yahoo.com>
2017-01-08 22:03:48 -0500

    Initial Commit

---
+++ Reps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reps
+//
+//  Created by Kevin Novak on 1/8/17.
+//  Copyright © 2017 Kevin Novak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
