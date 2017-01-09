e6ab84576ea83bbf9b7218c8527c4b281aae4162
Kevin Novak <kevinmnovak@yahoo.com>
2017-01-08 22:24:53 -0500

    Getting data

--- Reps/ViewController.swift
+++ Reps/ViewController.swift
@@ -13,6 +13,27 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let urlString = "https://httpbin.org/ip"
+        
+        let url = URL(string: urlString)
+        URLSession.shared.dataTask(with:url!) { (data, response, error) in
+            if error != nil {
+                print(error ?? "Error")
+            } else {
+                do {
+                    
+                    let parsedData = try JSONSerialization.jsonObject(with: data!, options: []) as! [String:String]
+                    let ip = parsedData["origin"]
+                    print("ip: " + ip!)
+
+                } catch let error as NSError {
+                    print(error)
+                }
+            }
+            
+            }.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
