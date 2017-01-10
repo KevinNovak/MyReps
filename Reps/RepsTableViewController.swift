7c35d3f4c63814b7750ae9c822b5aeb8192e045a
Kevin Novak <kevinmnovak@yahoo.com>
2017-01-10 18:20:09 -0500

    More detailed titles

--- Reps/RepsTableViewController.swift
+++ Reps/RepsTableViewController.swift
@@ -32,34 +32,77 @@ class RepsTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "repCell", for: indexPath) as! RepsTableViewCell
 
+        // image
+        cell.repImage.image = UIImage(named:"default_rep_image.png")
+        if let bioGuideID = reps[indexPath.row]["bioguide_id"] as? String {
+            let repImageURLString = "https://raw.githubusercontent.com/unitedstates/images/gh-pages/congress/225x275/" + bioGuideID + ".jpg"
+            let repImageURL = URL(string: repImageURLString)
+            if let data = try? Data(contentsOf: repImageURL!) {
+                if let image = UIImage(data: data) {
+                    cell.repImage.image = image
+                }
+            }
+        }
+        
+        // name
         if let firstName = reps[indexPath.row]["first_name"] as? String {
             if let lastName = reps[indexPath.row]["last_name"] as? String {
                 let fullName = firstName + " " + lastName
                 cell.repNameLabel.text = fullName
+            }
+        }
+        
+        // state
+        var stateString = "Unknown"
+        if let state = reps[indexPath.row]["state_name"] as? String {
+            stateString = state
+        }
+        
+        // party
+        var partyString = "Unknown"
+        if let party = reps[indexPath.row]["party"] as? String {
+            partyString = party
+        }
+        
+        // chamber
+        var chamberString = "Unknown"
+        var districtString = ""
+        if let chamber = reps[indexPath.row]["chamber"] as? String {
+            if (chamber == "senate") {
+                chamberString = "Senator"
+            } else if (chamber == "house") {
+                chamberString = "Representative"
                 
-                cell.repChamberLabel.text = "Unknown"
-                if let chamber = reps[indexPath.row]["chamber"] as? String {
-                    if (chamber == "senate") {
-                        cell.repChamberLabel.text = "Senate"
-                    } else if (chamber == "house") {
-                        cell.repChamberLabel.text = "House of Representatives"
-                    }
-                }
-                
-                cell.repImage.image = UIImage(named:"default_rep_image.png")
-                
-                if let bioGuideID = reps[indexPath.row]["bioguide_id"] as? String {
-                    let repImageURLString = "https://raw.githubusercontent.com/unitedstates/images/gh-pages/congress/225x275/" + bioGuideID + ".jpg"
-                    let repImageURL = URL(string: repImageURLString)
-                    if let data = try? Data(contentsOf: repImageURL!) {
-                        if let image = UIImage(data: data) {
-                            cell.repImage.image = image
-                        }
-                    }
+                // district
+                if let district = reps[indexPath.row]["district"] as? Int {
+                    districtString = String(district)
+                    print("district: " + String(district))
+                } else {
+                    print("FAIL")
                 }
             }
         }
-
+        
+        // full title
+        var fullTitle = "Unknown"
+        if (chamberString == "Senator") {
+            fullTitle = stateString + " " + chamberString + " (" + partyString + ")"
+        } else if (chamberString == "Representative") {
+            
+            var districtPostFix = "th"
+            if (districtString == "1") {
+                districtPostFix = "st"
+            } else if (districtString == "2") {
+                districtPostFix = "nd"
+            } else if (districtString == "3") {
+                districtPostFix = "rd"
+            }
+            
+            fullTitle = chamberString + " of " + stateString + "'s " + districtString + districtPostFix + " District (" + partyString + ")"
+        }
+        
+        cell.repChamberLabel.text = fullTitle
+        
         return cell
     }
 
